f2b3434fda192e135d34e1e208d3d9cead91e6ba
Alicia Washington <aliciawashington874@gmail.com>
2019-11-03 10:34:26 -0500

    Implemented pull to refresh on the home page.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -12,14 +12,20 @@ class HomeTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
     var numberofTweets: Int!
+    
+    let myRefreshControl = UIRefreshControl()
    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
         
+        myRefreshControl.addTarget( self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        
     }
     
-    func loadTweets(){
+    @objc func loadTweets(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": 10]
@@ -32,6 +38,7 @@ class HomeTableViewController: UITableViewController {
                     self.tweetArray.append(tweet)
                  }
                 self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
         }, failure: { (Error) in
             print("There was an error retrieving tweets ")
         })
