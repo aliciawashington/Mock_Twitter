42daaf7f1db0983f397376122b954a8984c8aecd
Alicia Washington <aliciawashington874@gmail.com>
2019-11-08 16:06:16 -0500

    Added capability to favorite and unfavorite a tweet.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,11 +17,16 @@ class HomeTableViewController: UITableViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
-        
         myRefreshControl.addTarget( self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
         
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
         
     }
     
@@ -67,6 +72,13 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+     
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        
+        
+        
         
         return cell
     }
